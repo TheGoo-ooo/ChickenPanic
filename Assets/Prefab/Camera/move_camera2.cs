40f52bcc8ec5e51223b4dc39389c36d4e9c6d147
florian.fasmeyer <florian.fasmeyer@he-arc.ch>
2018-05-04 03:31:44 +0200

    move_camera2 fix of move_camera that doesn't work on full screen nor on build versions or the game.

--- Assets/Prefab/Camera/move_camera2.cs
+++ Assets/Prefab/Camera/move_camera2.cs
@@ -4,35 +4,50 @@ using UnityEngine;
 
 public class move_camera2 : MonoBehaviour {
 
-	Vector2 mouseStart = new Vector2(0,0);
-	// Use this for initialization
-	void Start () {
+	public float moveSpeed = 0.3f;
+	public float borderRange = 0.18f;
+	private float borderRangeX = 0f;
+	private float borderRangeY = 0f;
+	private bool move = false;
+	float x, y, z;
+
+	void Start(){
+		borderRangeX = Screen.width * borderRange;
+		borderRangeY = Screen.height * borderRange;
+	}
+
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+			move = true;
+		else if (Input.GetMouseButtonUp(0))
+			move = false;
+
+		if (move)
+			moveCamera ();
 
 	}
 
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetMouseButtonDown(0))
-		{
-			mouseStart.x = Input.mousePosition.x;
-			mouseStart.y = Input.mousePosition.y;
+	private void moveCamera (){
+		if (Input.mousePosition.x < borderRangeX) {
+			x = -1 * moveSpeed;
+		} else if (Input.mousePosition.x > Screen.width - borderRangeX)
+			x = 1 * moveSpeed;
+		else
+			x = 0;
 
-		}
-		if(Input.GetMouseButtonUp(0))
-		{
-			mouseStart.x = 0;
-			mouseStart.y = 0;
-		}
-		if(mouseStart.x != 0 && Camera.current != null)
-		{
-			//Camera.current.transform.Translate(new Vector3((Input.mousePosition.x - mouseStart.x)*moveSpeed, 0.0f, (mouseStart.y - Input.mousePosition.y)*moveSpeed));
-			/*
-			 Camera.current.transform.position = 
-				new Vector3(
-					Camera.current.transform.position.x + (Input.mousePosition.x - mouseStart.x)*moveSpeed, 
-					Camera.current.transform.position.y, 
-					Camera.current.transform.position.z + (Input.mousePosition.y - mouseStart.y)*moveSpeed);
-			*/
+		if (Input.mousePosition.y < borderRangeY)
+			y = -1 * moveSpeed;
+		else if (Input.mousePosition.y > Screen.height - borderRangeY)
+			y = 1 * moveSpeed;
+		else
+			y = 0;
+
+		if (Camera.current != null) {
+			Camera.current.transform.position = 
+				new Vector3 (
+				Camera.current.transform.position.x + x, 
+				Camera.current.transform.position.y, 
+				Camera.current.transform.position.z + y);
 		}
 	}
 }
