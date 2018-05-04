fb37a2a81e87fd4798597f1149a1e1f80de41d1f
florian.fasmeyer <florian.fasmeyer@he-arc.ch>
2018-05-04 02:51:34 +0200

    Fix menu, credits, 3 tutorial levels

---
+++ Assets/Prefab/rotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+	public Vector3 rotation = new Vector3(0.0f,0.0f,0.0f);
+
+	void Update () {
+		transform.Rotate (rotation);
+
+	}
+}
